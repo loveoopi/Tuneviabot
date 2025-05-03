a2496d8e8f14dbd0414d984b8a0b9ccac2f95073
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-05-03 23:54:49 +0530

    repo.py

--- Tune/plugins/bot/repo.py
+++ Tune/plugins/bot/repo.py
@@ -25,7 +25,7 @@ async def show_repo(_, msg):
         ],
         [
             InlineKeyboardButton("🛠️ ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/+FkwrzozCChkwNGU1"),
-            InlineKeyboardButton("🎵 ɢɪᴛʜᴜʙ", url="https://github.com/CBot")
+            InlineKeyboardButton("🎵 ɢɪᴛʜᴜʙ", url="https://github.com/CertifiedCoders/TuneViaBot")
         ]
     ]
 
